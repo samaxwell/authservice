82355cada11fcacdc71f130a7e637be1c194b46a
Sean Maxwell <katanablaydz@yahoo.com>
2019-09-19 22:05:00 -0500

    Updated /user endpoint

--- src/main/java/com/sean/OauthSvrApplication.java
+++ src/main/java/com/sean/OauthSvrApplication.java
@@ -9,6 +9,7 @@ import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,9 +27,10 @@ public class OauthSvrApplication {
 		SpringApplication.run(OauthSvrApplication.class, args);
 	}
 	
-	@RequestMapping(value = { "/user "}, produces = "application/json")
+	@GetMapping("/user")
 	public Map<String, Object> user(OAuth2Authentication user) {
 		
+		System.out.println("Checking token");
 		Map<String, Object> userInfo = new HashMap<>();
 		userInfo.put(
 				"user",
